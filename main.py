9c962e1a1819ac0f56c29739079c00f830c91df6
Bence Kalmar <bkalmar1996@gmail.com>
2021-08-04 02:48:00 +0200

    feat: add log calls to keep track of progress

--- main.py
+++ main.py
@@ -24,29 +24,37 @@ except ImportError:
 # each new execution.
 token_file = "token.json"
 
+logging.info(f"connect to Duolingo API ...")
 duo = duolingo.Duolingo(
     username=config.username,
     password=config.password,
     session_file=token_file,
 )
 
+logging.info(f"get language abbreviation ...")
 language_abbr = duo.get_abbreviation_of(config.language)
+logging.info(f"language abbreviation: {language_abbr!r}")
 
 phrases = set()
 
 if config.include_vocabulary:
+    logging.info(f"get vocabulary ...")
     vocabulary = duo.get_vocabulary(language_abbr)
     phrases.update(v["word_string"] for v in vocabulary["vocab_overview"])
+    logging.info(f"vocabulary phrases: {len(phrases)}")
 
 if config.include_words:
+    logging.info(f"get known words ...")
     words = duo.get_known_words(language_abbr)
     phrases.update(words)
+    logging.info(f"known words phrases: {len(words)}")
 
 phrases = sorted(phrases)
 
 # Print phrases to out stream in Pleco flashcard text file format:
 # https://android.pleco.com/manual/240/flash.html#textformat
 
+logging.info(f"write output ...")
 out = sys.stdout
 
 # Switch to the flashcard category, if any.
@@ -61,3 +69,5 @@ for phrase in phrases:
     #
     #   phrase [ TAB pinyin [ TAB definition ] ]
     print(f"{phrase}", file=out)
+
+logging.info(f"total phrases: {len(phrases)}")
